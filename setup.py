97a3ed3eb560625119dafdbd39b4a25edd2e72b1
minamikik <mia@sync.dev>
2022-12-16 12:22:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torch-deep-danbooru',
-    version='1',
+    version='0.0.1',
     description='',
     packages=find_packages(),
     install_requires=[
